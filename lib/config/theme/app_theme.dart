38d1eb88e26cf5840dc80e0b51905a96485dacfa
Diego Gutierrez <diegogucd@gmail.com>
2023-07-12 07:38:35 -0600

    Commit1_12_07_23

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -4,3 +4,12 @@ class AppTheme {
   ThemeData getTheme() =>
       ThemeData(useMaterial3: true, colorSchemeSeed: const Color(0xff2862f5));
 }
+
+/**
+ * Material 3 es la versión más reciente del sistema de diseño de código abierto de Google. 
+ *     Se expandió la compatibilidad de Flutter para compilar aplicaciones estéticamente agradables con 
+ *     Material 3.
+ * El tema tiene un esquema de color, ThemeData.colorScheme, que puede pasarse como un parámetro al 
+ *     constructor o mediante los parámetros 'brightness' y 'colorSchemeSeed' (que se usan para generar 
+ *     un esquema con ColorScheme.fromSeed).
+ */
