d57b7691427a0f8e481666000b05e10c6d715cbd
DiegoYair <diegogucd@gmail.com>
2023-08-11 14:04:07 -0600

    pruebav1

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+/*INICIO*/
+
 Future<void> main() async {
   /*
     Un Future representa principalmente el resultado de
@@ -33,6 +35,3 @@ class MainApp extends StatelessWidget {
     );
   }
 }
-/**
- * Hola se hizo el commit
- */
